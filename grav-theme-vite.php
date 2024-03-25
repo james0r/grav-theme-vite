092cff2053c13a283de46d12557e9d9efd847e29
James <james.auble@gmail.com>
2024-03-25 20:04:56 +0900

    added public folder for dev

--- grav-theme-vite.php
+++ grav-theme-vite.php
@@ -5,5 +5,26 @@ use Grav\Common\Theme;
 
 class GravThemeVite extends Theme
 {
-    // Access plugin events in this class
+  public static function getSubscribedEvents(): array {
+    return [
+      'onPluginsInitialized' => ['onPluginsInitialized', 0],
+    ];
+  }
+
+  /**
+   * Initialize the plugin
+   */
+  public function onPluginsInitialized(): void {
+    // Don't proceed if we are in the admin plugin
+    if ($this->isAdmin()) {
+      return;
+    }
+
+    // Enable the main events we are interested in
+    $this->enable(
+      [
+        'onTwigTemplatePaths' => ['onTwigTemplatePaths', 1000],
+      ]
+    );
+  }
 }
